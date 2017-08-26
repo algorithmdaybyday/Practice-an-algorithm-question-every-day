0b11aa00182535ca2425eba136f841120c9946a7
wilsonandiphone@icloud.com <wilsonandiphone@icloud.com>
2017-08-25 22:34:22 -0700

    add examples

--- chapter_Tree/leetcode/bstLevelOrderTraversal.java
+++ chapter_Tree/leetcode/bstLevelOrderTraversal.java
@@ -1,5 +1,4 @@
-/**Source: https://leetcode.com/problems/binary-tree-level-order-traversal/#/solutions
- * 问题链接：https://leetcode.com/problems/binary-tree-level-order-traversal/#/description
+/**
  * Definition for a binary tree node.
  * public class TreeNode {
  *     int val;
@@ -10,20 +9,25 @@
  */
 public class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        Queue<TreeNode> queue = new LinkedList<TreeNode>();
-	List<List<Integer>> wrapList = new LinkedList<List<Integer>>();
-	if(root==null) return wrapList;
-	queue.offer(root);
-	while(!queue.isEmpty()){
-		int levelNum = queue.size();
-		List<Integer>subList = new LinkedList<Integer>();
-		for(int i = 0; i<levelNum; i++){
-			if(queue.peek().left != null) queue.offer(queue.peek().left);
-			if(queue.peek().right != null) queue.offer(queue.peek().right);
-			subList.add(queue.poll().val);
-    }
-    wrapList.add(subList);
-	}
-	return wrapList;
+    List<List<Integer>> res = new ArrayList<>();  
+    if (root == null) return res;  
+    Queue<TreeNode> queue = new LinkedList<>();  
+    queue.add(root);  
+    while (!queue.isEmpty()) {  
+        List<Integer> level = new ArrayList<>();
+      int cnt = queue.size();  
+      for (int i = 0; i < cnt; i++) {  
+        TreeNode node = queue.poll();  
+        level.add(node.val);  
+        if (node.left != null) {  
+          queue.add(node.left);  
+        }
+        if (node.right != null) {  
+          queue.add(node.right);  
+        }  
+      }  
+      res.add(level);   
+    }  
+    return res;
     }
 }
